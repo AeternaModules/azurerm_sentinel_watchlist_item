0169db0e5622779831fcda3772df30edc600b246
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_watchlist_items_id" {
+  description = "Map of id values across all sentinel_watchlist_items, keyed the same as var.sentinel_watchlist_items"
+  value       = { for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : k => v.id }
+}
 output "sentinel_watchlist_items_name" {
   description = "Map of name values across all sentinel_watchlist_items, keyed the same as var.sentinel_watchlist_items"
   value       = { for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : k => v.name }
