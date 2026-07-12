f87d46bf3ee576f0e97923cdf16d895599bac266
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_watchlist_items" {
-  description = "All sentinel_watchlist_item resources"
-  value       = azurerm_sentinel_watchlist_item.sentinel_watchlist_items
-}
 output "sentinel_watchlist_items_name" {
-  description = "List of name values across all sentinel_watchlist_items"
-  value       = [for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : v.name]
+  description = "Map of name values across all sentinel_watchlist_items, keyed the same as var.sentinel_watchlist_items"
+  value       = { for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : k => v.name }
 }
 output "sentinel_watchlist_items_properties" {
-  description = "List of properties values across all sentinel_watchlist_items"
-  value       = [for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : v.properties]
+  description = "Map of properties values across all sentinel_watchlist_items, keyed the same as var.sentinel_watchlist_items"
+  value       = { for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : k => v.properties }
 }
 output "sentinel_watchlist_items_watchlist_id" {
-  description = "List of watchlist_id values across all sentinel_watchlist_items"
-  value       = [for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : v.watchlist_id]
+  description = "Map of watchlist_id values across all sentinel_watchlist_items, keyed the same as var.sentinel_watchlist_items"
+  value       = { for k, v in azurerm_sentinel_watchlist_item.sentinel_watchlist_items : k => v.watchlist_id }
 }
 
